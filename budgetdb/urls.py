fffc52f242f68d86a339f8545a26ea91fb023425
Mathieu Fortin <mathieugfortin@gmail.com>
2021-03-30 21:23:57 -0400

    good first draft of category pie chart

--- budgetdb/urls.py
+++ budgetdb/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     path('chart/', views.FirstGraph.as_view(), name='line_chart'),
     path('chartJSON', views.FirstGraphJSON.as_view(), name='line_chart_json'),
     path('cat1/pie-chart/<int:cat_type_pk>', views.CatTotalChart.as_view(), name='cat1_pie'),
-    path('catTotalJSON', views.GetCat1TotalChartData, name='cat1_pie_json'),
-    path('catTotalJSON', views.GetCat2TotalChartData, name='cat2_pie_json'),
+    path('cat1TotalJSON', views.GetCat1TotalChartData, name='cat1_pie_json'),
+    path('cat2TotalJSON', views.GetCat2TotalChartData, name='cat2_pie_json'),
 
     # Cat1
     path('cat1/', views.Cat1ListView.as_view(),
